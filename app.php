574bf7159df57d7238801572f407d59cd30776ad
rahullahoria <rahul_lahoria@yahoo.com>
2016-06-04 16:49:10 +0530

    added slim response heades

--- app.php
+++ app.php
@@ -14,10 +14,10 @@ if(!isset($app))
     $app = new \Slim\Slim();
 
 
-$app->response->headers->set('Access-Control-Allow-Origin',  '*');
+/*$app->response->headers->set('Access-Control-Allow-Origin',  '*');
 $app->response->headers->set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 $app->response->headers->set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-$app->response->headers->set('Content-Type', 'application/json');
+$app->response->headers->set('Content-Type', 'application/json');*/
 
 /* Starting routes */
 
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once "header.php";
+
 include 'db.php';
 require 'Slim/Slim.php';
 
