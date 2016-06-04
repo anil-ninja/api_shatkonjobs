8fac6bd24333b1d65add4e56a31dac1b88e0498b
rahullahoria <rahul_lahoria@yahoo.com>
2016-06-04 17:01:34 +0530

    added slim response heades

--- app.php
+++ app.php
@@ -15,6 +15,7 @@ if(!isset($app))
 
 
 $app->response->headers->set('Access-Control-Allow-Origin',  $_SERVER['HTTP_ORIGIN']);
+$app->response->headers->set('Access-Control-Allow-Credentials',  'true');
 /*$app->response->headers->set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 $app->response->headers->set('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 */
