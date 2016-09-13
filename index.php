660f81891738fe567bc7fa6c6ef4c4d1e8368a84
aditi2016 <aditilahoria@gmail.com>
2016-09-13 16:49:45 +0530

    right

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+require_once "header.php";
+
+include 'db.php';
+require 'Slim/Slim.php';
+
+
+//candidates resource
+require_once "resources/candidates/deleteCandidate.php";
+require_once "resources/candidates/getCandidatesToCall.php";
+require_once "resources/candidates/insertCandidate.php";
+require_once "resources/candidates/searchCandidates.php";
+require_once "resources/candidates/updateCandidate.php";
+require_once "resources/auth/postUserAuth.php";
+
+//professions resource
+require_once "resources/professions/getAllProfessions.php";
+
+//areas resource
+require_once "resources/areas/getAllAreas.php";
+
+//app
+require_once "app.php";
+
+
+
+
+?>
